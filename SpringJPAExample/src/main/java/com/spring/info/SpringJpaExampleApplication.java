af3dfbb43790fb9dbd691d19bd3e251ddfc93e3c
Ramaiah Pillala <ramaiahpillala@gmail.com>
2020-04-15 21:11:23 +0530

    commiting Adding internalization code

--- SpringJPAExample/src/main/java/com/spring/info/SpringJpaExampleApplication.java
+++ SpringJPAExample/src/main/java/com/spring/info/SpringJpaExampleApplication.java
@@ -1,7 +1,13 @@
 package com.spring.info;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class SpringJpaExampleApplication {
@@ -10,4 +16,21 @@ public class SpringJpaExampleApplication {
 		SpringApplication.run(SpringJpaExampleApplication.class, args);
 	}
 
+	/**
+	 * implementing internalization concept I18
+	 * 
+	 * 
+	 */
+	
+	  @Bean public LocaleResolver localeResolver() { SessionLocaleResolver
+	  localeResolver = new SessionLocaleResolver();
+	  localeResolver.setDefaultLocale(Locale.US); return localeResolver;
+	  
+	  }
+	  
+	  @Bean public ResourceBundleMessageSource bundleMessageSource() {
+	  ResourceBundleMessageSource messageSource = new
+	  ResourceBundleMessageSource(); messageSource.setBasename("messages"); return
+	  messageSource; }
+	 
 }
--- SpringJPAExample/src/main/java/com/spring/info/controller/HelloWorldController.java
+++ SpringJPAExample/src/main/java/com/spring/info/controller/HelloWorldController.java
@@ -1,21 +1,30 @@
 package com.spring.info.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.info.beans.HelloWorldBean;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+	private MessageSource messageSource;
 
-	// its regarding returing Bean class
-	// note: here we can use @ReqestMapping(Mehthod =RequestMethod.GET,path =
-	// "/helloWorld") also
+	/**
+	 * its regarding returing Bean class note: here we can
+	 * use @ReqestMapping(Mehthod =RequestMethod.GET,path = "/helloWorld") also
+	 * 
+	 * @return
+	 */
 
 	@GetMapping(path = "/helloWorld")
 	public HelloWorldBean beanReturn() {
-
 		return new HelloWorldBean("Hello , here we are retuning bean object");
 
 	}
@@ -24,6 +33,12 @@ public class HelloWorldController {
 	@GetMapping("/helloWorld/path/{name}")
 	public HelloWorldBean pathVaribleInfo(@PathVariable String name) {
 		return new HelloWorldBean("pathVArible  : " + String.format("value would be, %s", name));
+	}
+
+	@GetMapping(path = "/internalizationCheck")
+	public String internalizationExample(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.messeges", null, locale);
+
 	}
 
 }
